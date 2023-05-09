59a82fc1113709f76c6a9f8f1d630dc6389a4687
zhanglabs <zhanglabs@zhanglab.ccmb.med.umich.edu>
2023-05-09 15:53:55 -0400

    blastdb

---
+++ script/formatdb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FILE=`readlink -e $0`
+bindir=`dirname $FILE`
+rootdir=`dirname $bindir`
+
+cat $rootdir/data/nonredundant/seqs_longer.fasta $rootdir/data/nonredundant/seqs_shorter.fasta | sed 's/;\t/\t>/g'|cut -f2|$bindir/fasta2tsv - - |sort|uniq| $bindir/tsv2fasta - $rootdir/data/nonredundant/seq.fasta 
+$bindir/makeblastdb -in $rootdir/data/nonredundant/seq.fasta -dbtype prot -out $rootdir/data/nonredundant/seq.fasta
